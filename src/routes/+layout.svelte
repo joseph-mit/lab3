dd08851a7795d9d553a04bd07bf161f2561b8a6d
joseph-mit <josephfn@mit.edu>
2026-03-05 03:21:11 -0500

    updated

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,12 @@
 <script>
   import { base } from "$app/paths";
+  import { page } from "$app/stores";
 
   let pages = [
     { url: "/", title: "Home" },
     { url: "/projects", title: "Projects" },
+    { url: "/resume", title: "resume" },
     { url: "/contact", title: "Contact" },
-    { url: "/resume", title: "Resume" },
     { url: "https://github.com/joseph-mit", title: "GitHub" }
   ];
 </script>
@@ -15,6 +16,16 @@
     <a
       href={p.url.startsWith("http") ? p.url : base + p.url}
       target={p.url.startsWith("http") ? "_blank" : null}
+      rel={p.url.startsWith("http") ? "noreferrer" : null}
+      class:current={
+        p.url.startsWith("http")
+          ? false
+          : p.url === "/"
+            ? $page.url.pathname === (base + "/") ||
+              $page.url.pathname === base ||
+              $page.url.pathname === "/"
+            : $page.url.pathname.startsWith(base + p.url)
+      }
     >
       {p.title}
     </a>
